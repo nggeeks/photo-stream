38b3340d0bb4be9702cf3314ae69b3f6d72d4927
Bolaji Adeyemi <d.bola1@gmail.com>
2021-03-25 19:19:24 +0000

    Update Readme

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const apiKey = '39ea7e30281d90fb9fbf4c9c00490bf9';
+export const apiKey = ' '; // Add your flickr Apikey here
 export const flickrPhotoSearchUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.search';
 export const flickrPhotoInfoUrl = 'https://www.flickr.com/services/rest/?method=flickr.photos.getInfo';
  
